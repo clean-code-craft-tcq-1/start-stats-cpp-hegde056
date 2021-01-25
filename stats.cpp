dd82d6b2e28bca4ad16cf9dfc0a72fa5d1b3c511
Sudarshan Hegde <suduhegde01@gmail.com>
2021-01-25 11:26:36 +0530

    Implementation for Testcase 3

--- stats.cpp
+++ stats.cpp
@@ -38,3 +38,43 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& numVect) {
     }
     
 }
+
+EmailAlert::EmailAlert() 
+{ 
+    emailSent = false; 
+}
+
+void EmailAlert::SetAlert()
+{
+    this->emailSent = true;
+}
+
+LEDAlert::LEDAlert() 
+{ 
+    ledGlows = false; 
+}
+
+void LEDAlert::SetAlert()
+{
+    this->ledGlows = true; 
+}
+
+StatsAlerter::StatsAlerter(const float m , std::vector<IAlerter*>& a)
+    :maxTh(m) , alerter(a)
+{}
+
+void StatsAlerter::checkAndAlert(const std::vector<float>& numVect)
+{
+    for (auto i : numVect)
+    {
+        if (i > maxTh)
+        { 
+            for (int j = 0; j < alerter.size(); ++j)
+            {
+                this->alerter.at(j)->SetAlert();
+            }
+            break;
+        }
+    }
+}
+
--- stats.h
+++ stats.h
@@ -8,3 +8,39 @@ typedef struct
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>&);
 }
+
+class IAlerter
+{
+public:
+    virtual void SetAlert() = 0;
+};
+
+class EmailAlert : public IAlerter
+{
+public:
+    bool emailSent;
+    EmailAlert();
+    void SetAlert();
+};
+
+class LEDAlert : public IAlerter
+{
+public:
+    bool ledGlows;
+    LEDAlert();
+    void SetAlert();
+};
+
+class StatsAlerter
+{
+private : 
+    float maxTh; 
+    std::vector<IAlerter*> alerter; 
+
+public : 
+    StatsAlerter(const float , std::vector<IAlerter*>&);
+
+    void checkAndAlert(const std::vector<float>&);
+};
+
+
